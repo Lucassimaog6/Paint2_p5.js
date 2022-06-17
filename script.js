7a873ab385f278a89f0b9df9e3527bf3e6159cc7
Lucassimaog6 <22101918@aluno.cotemig.com.br>
2022-06-17 11:22:42 -0300

    Aleração no Design conforme  mockup feito no FIgma

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
+let tamValue;
+let colValue;
+
 function setup() {
-    let canvas = document.getElementById("canvas")
-    let canvasW = canvas.offsetWidth
-    let canvasH = canvas.offsetHeight
-    let c =createCanvas(canvasW,canvasH)
-    c.parent("canvas")
-    background(255,255,255)
+  let canvas = document.getElementById("canvas");
+  let canvasW = canvas.clientWidth;
+  let canvasH = canvas.clientHeight;
+  let c = createCanvas(canvasW, canvasH + 100);
+  c.parent("canvas");
+  background(255, 255, 255);
+}
+
+function draw() {
+  tamValue = document.getElementById("sliderTam").value;
+  colValue = document.getElementById("colorCol").value;
+  if (mouseIsPressed) {
+    stroke(colValue);
+    strokeWeight(tamValue);
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
-  
-  function draw() {  
-    if(mouseIsPressed){
-        stroke(10)
-        strokeWeight(10)
-        line(mouseX, mouseY, pmouseX, pmouseY)
-    }
+}
+
+function keyPressed() {
+  if (key === 'l') {
+    limpar();
   }
+  if (key === 's') {
+    salvar();
+  }
+}
+
+function limpar() {
+  background(255, 255, 255);
+}
+function salvar() {
+  saveCanvas("Paint_Creation", "png");
+}
